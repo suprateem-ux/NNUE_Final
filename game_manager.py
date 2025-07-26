aa451de4f4d45bee9c2f255c8a90156b9065171d
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-07-26 10:05:29 +0530

    Update game_manager.py

--- game_manager.py
+++ game_manager.py
@@ -12,10 +12,11 @@ from matchmaking import Matchmaking
 
 
 class Game_Manager:
-    def __init__(self, api: API, config: Config, username: str, engine) -> None:
+    def __init__(self, api: API, config: Config, username: str) -> None:
         self.api = api
         self.config = config
         self.username = username
+
         self.challenger = Challenger(api)
         self.changed_event = Event()
         self.matchmaking = Matchmaking(api, config, username)
