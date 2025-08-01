221872a430316257e868fb47d564daec48a906d1
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-08-01 16:53:20 +0530

    Update botli_dataclasses.py

--- botli_dataclasses.py
+++ botli_dataclasses.py
@@ -121,6 +121,37 @@ class Game_Information:
     def from_gameFull_event(cls, gameFull_event: dict[str, Any]) -> 'Game_Information':
         assert gameFull_event['type'] == 'gameFull'
 
+        raw_state = gameFull_event['state']
+        if isinstance(raw_state, str):
+            import json
+            state = json.loads(raw_state)
+        else:
+            state = raw_state
+
+        return cls(
+            id_=gameFull_event['game']['id'],
+            white_title=gameFull_event['white'].get('title'),
+            white_name=gameFull_event['white']['name'],
+            white_rating=gameFull_event['white'].get('rating'),
+            white_ai_level=gameFull_event['white'].get('aiLevel'),
+            white_provisional=gameFull_event['white'].get('provisional', False),
+            black_title=gameFull_event['black'].get('title'),
+            black_name=gameFull_event['black']['name'],
+            black_rating=gameFull_event['black'].get('rating'),
+            black_ai_level=gameFull_event['black'].get('aiLevel'),
+            black_provisional=gameFull_event['black'].get('provisional', False),
+            initial_time_ms=gameFull_event['clock']['initial'],
+            increment_ms=gameFull_event['clock']['increment'],
+            speed=gameFull_event['speed'],
+            rated=gameFull_event['rated'],
+            variant=Variant.from_str(gameFull_event['variant']['key']),
+            variant_name=gameFull_event['variant']['name'],
+            initial_fen=gameFull_event.get('initialFen', ''),
+            fen_str=gameFull_event.get('initialFen', ''),
+            state=state
+        )
+
+
         id_ = gameFull_event['id']
         white_title = gameFull_event['white'].get('title')
         white_name = gameFull_event['white'].get('name', 'AI')
