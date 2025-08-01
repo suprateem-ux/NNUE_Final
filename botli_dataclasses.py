51d7a8cd1ef267f1ea6ca406a21b3546e1866e9f
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2025-08-01 17:12:25 +0530

    Update botli_dataclasses.py

--- botli_dataclasses.py
+++ botli_dataclasses.py
@@ -131,12 +131,12 @@ class Game_Information:
         return cls(
             id_=gameFull_event['id'],
             white_title=gameFull_event['white'].get('title'),
-            white_name=gameFull_event['white']['name'],
+            white_name=gameFull_event['white'].get('name', 'AI'),
             white_rating=gameFull_event['white'].get('rating'),
             white_ai_level=gameFull_event['white'].get('aiLevel'),
             white_provisional=gameFull_event['white'].get('provisional', False),
             black_title=gameFull_event['black'].get('title'),
-            black_name=gameFull_event['black']['name'],
+            black_name=gameFull_event['black'].get('name', 'AI'),
             black_rating=gameFull_event['black'].get('rating'),
             black_ai_level=gameFull_event['black'].get('aiLevel'),
             black_provisional=gameFull_event['black'].get('provisional', False),
@@ -144,7 +144,7 @@ class Game_Information:
             increment_ms=gameFull_event['clock']['increment'],
             speed=gameFull_event['speed'],
             rated=gameFull_event['rated'],
-            variant = Variant(gameFull_event['variant']['key'])
+            variant=Variant(gameFull_event['variant']['key']),  # ✅ This is correct
             variant_name=gameFull_event['variant']['name'],
             initial_fen=gameFull_event.get('initialFen', ''),
             fen_str=gameFull_event.get('initialFen', ''),
